e073aae6526412664fa0b016e97bc70dcff863da
Ryan Tan <ryan_tan@ida.gov.sg>
2016-03-09 11:02:58 +0800

    add jshint options for es6

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 //using mjackson's expect library
 import expect from 'expect';
 
-describe ('test', () => {
+describe ('base', () => {
   it('should pass basic test case', () => {
     expect(true).toBe(true);
   });
