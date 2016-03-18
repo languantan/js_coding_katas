986026a26b49effe60a7915acc560cc0862e7713
Ryan Tan <ryan_tan@ida.gov.sg>
2016-03-18 15:05:08 +0800

    added function under test (fut.js)

--- test/test.js
+++ test/test.js
@@ -1,8 +1,13 @@
 //using mjackson's expect library
 import expect from 'expect';
+// import /*somethin*/ from '../fut.js';
 
-describe ('base', () => {
-  it('should pass basic test case', () => {
+describe ('True', () => {
+  it('should be true', ()=> {
     expect(true).toBe(true);
+  });
+
+  it('should not be false', ()=> {
+    expect(true).toNotBe(false);
   });
 });
