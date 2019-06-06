943be417f799dbfbf5907ab63d13afc24a40d8ee
Ryan Tan <languantan@gmail.com>
2019-06-07 00:23:05 +0800

    update syntax and lint errors

--- fut.js
+++ fut.js
@@ -1,3 +1,51 @@
-export default function (truthValue) {
-  return !truthValue;
+export default class {
+  constructor() {
+    this.words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+      'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    this.words[20] = ['twenty'];
+    this.words[30] = ['thirty'];
+    this.words[40] = ['forty'];
+    this.words[50] = ['fifty'];
+    this.words[60] = ['sixty'];
+    this.words[70] = ['seventy'];
+    this.words[80] = ['eighty'];
+    this.words[90] = ['ninety'];
+  }
+
+  convertToString(num) {
+    let finalString = '';
+
+    const tens = num % 100;
+    if (tens < 10 && num > 100) {
+      finalString += `and ${this.words[tens]}`;
+    } else if (tens < 20) {
+      finalString += this.words[tens];
+    } else {
+      finalString += this.handleTens(tens);
+    }
+
+    const hundreds = Math.floor(num / 100);
+    if (hundreds === 0) return finalString;
+    finalString = this.handleHundredsAndThousands(hundreds, finalString, 'hundred');
+
+    const thousands = Math.floor(num / 1000);
+    if (thousands === 0) return finalString;
+    finalString = this.handleHundredsAndThousands(thousands, finalString, 'thousand');
+
+    return finalString;
+  }
+
+  handleTens(num) {
+    const ones = num % 10;
+    const tens = (num % 100) - ones;
+
+    return `${this.words[tens]} ${this.words[ones]}`;
+  }
+
+  handleHundredsAndThousands(num, finalString, unitString) {
+    if (num % 10 === 0) {
+      return `and ${finalString}`;
+    }
+    return `${this.words[num % 10]} ${unitString} ${finalString}`;
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,13 +1,31 @@
 //using mjackson's expect library
 import expect from 'expect';
-import Negate from '../fut.js';
+import Converter from '../fut.js';
 
-describe ('Negate', () => {
-  it('should change the truth', ()=> {
-    expect(Negate(true)).toBe(false);
-  });
+describe('Converter', () => {
+  let converter = new Converter();
 
-  it('should not retain truth', ()=> {
-    expect(Negate(true)).toNotBe(true);
+  describe('#convertToString', () => {
+    it('should convert ones and tens', () => {
+      [{ 'test': 1, 'expected': 'one' },
+      { 'test': 10, 'expected': 'ten' },
+      { 'test': 12, 'expected': 'twelve' },
+      { 'test': 35, 'expected': 'thirty five' },
+      { 'test': 88, 'expected': 'eighty eight' },
+      ].forEach(v => {
+        expect(converter.convertToString(v.test)).toBe(v.expected);
+      });
+    });
+
+    it('should convert hundreds and thousands', () => {
+      [{ 'test': 99, 'expected': 'ninety nine' },
+      { 'test': 303, 'expected': 'three hundred and three' },
+      { 'test': 789, 'expected': 'seven hundred eighty nine' },
+      { 'test': 1026, 'expected': 'one thousand and twenty six' },
+      { 'test': 3621, 'expected': 'three thousand six hundred twenty one' },
+      ].forEach(v => {
+        expect(converter.convertToString(v.test)).toBe(v.expected);
+      });
+    });
   });
 });
